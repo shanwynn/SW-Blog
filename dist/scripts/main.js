195b17e6a6ba2d222bd56316228d3cd9fb83a5b8
Shannon Wynn <shannonwynn@mac.com>
2014-10-22 10:40:06 -0400

    heroku

---
+++ dist/scripts/main.js
@@ -0,0 +1,45 @@
+var blogTemplate = _.template($('.blog-posts').html());
+
+var apiUrl = "http://tiny-pizza-server.herokuapp.com/collections/SW-Blog";
+
+  $('input[type=submit]').on('click', function (event) {
+    event.preventDefault();
+
+var title =  $('input.field').serializeArray();
+var post =  $('textarea.blog').serializeArray();
+var date = new Date ();
+    blogObject['date'] = date;
+
+var blogObject = {};
+
+    title.forEach(function (field) {
+      blogObject[field.name] = field.value;
+    });
+
+    post.forEach(function (blog) {
+      blogObject[blog.name] = blog.value;
+    });
+
+    $.ajax({
+      method: 'POST',
+      url: apiUrl,
+      data: blogObject
+    }).done(function (data) {$('input.field').val('');
+    });
+  });
+
+  var previousCount = 0;
+
+  setInterval(function () {
+    $.ajax( {url: apiUrl} ).done(function (blogPosts) {
+      if(blogPosts.length > prevCount) {
+        prevCount = blogPosts.length;
+
+    var finishedTemplates = _.map(blogPosts, function (post) {
+      return blogTemplate(post);
+    });
+
+    $('.post-output').html(finishedTemplates);
+  });
+}, 1000);
+});
