48d6d1f98d27f324dd774964e91c399d70471344
Shannon Wynn <shannonwynn@mac.com>
2014-10-22 10:20:17 -0400

    setting up heroku server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,23 @@
 var blogTemplate = _.template($('.blog-posts').html());
-var postCountTemplate = _.template($('.user-sidebar-template').html());
 
 var apiUrl = "http://tiny-pizza-server.herokuapp.com/collections/SW-Blog";
-var blogObject = {};
 
   $('input[type=submit]').on('click', function (event) {
     event.preventDefault();
 
 var title =  $('input.field').serializeArray();
-var post =  $('textarea.field').serializeArray();
+var post =  $('textarea.blog').serializeArray();
 var date = new Date ();
     blogObject['date'] = date;
 
-    title.forEach(function (title) {
-      blogObject[title.name] = title.value;
+var blogObject = {};
+
+    title.forEach(function (field) {
+      blogObject[field.name] = field.value;
     });
 
-    post.forEach(function (post) {
-      blogObject[post.name] = post.value;
+    post.forEach(function (blog) {
+      blogObject[blog.name] = blog.value;
     });
 
     $.ajax({
@@ -28,18 +28,14 @@ var date = new Date ();
     });
   });
 
-    $.ajax( {url: apiUrl} ).done(function (posts) {
+  var previousCount = 0;
 
-    });
-
-
-/*  setInterval(function () {
+  setInterval(function () {
     $.ajax( {url: apiUrl} ).done(function (blogPosts) {
       if(blogPosts.length > prevCount) {
-        console.log(blogPosts);
         prevCount = blogPosts.length;
-*/
-    var finishedTemplates = _.map(posts, function (post) {
+
+    var finishedTemplates = _.map(blogPosts, function (post) {
       return blogTemplate(post);
     });
 
