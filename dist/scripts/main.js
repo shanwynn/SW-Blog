197b0a7b09480d3932dabee3ec83027f8711401a
Shannon Wynn <shannonwynn@mac.com>
2014-10-23 12:34:20 -0400

    release

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,45 +1,47 @@
-var blogTemplate = _.template($('.blog-posts').html());
+var template = _.template($('.blog-posts').html());
 
 var apiUrl = "http://tiny-pizza-server.herokuapp.com/collections/SW-Blog";
 
-  $('input[type=submit]').on('click', function (event) {
-    event.preventDefault();
+var BlogPost = function (attributes) {
+    return _.extend({
+      date: new Date(),
+      title: 'title',
+      post: 'post goes here'
+    }, attributes);
+};
 
-var title =  $('input.field').serializeArray();
-var post =  $('textarea.blog').serializeArray();
-var date = new Date ();
-    blogObject['date'] = date;
+$('input[type=submit]').on('click', function (event) {
+  event.preventDefault();
 
-var blogObject = {};
+  var fieldValues = $('input.field, textarea.field').serializeArray();
 
-    title.forEach(function (field) {
+  var blogObject = new BlogPost();
+
+  fieldValues.forEach(function (field) {
       blogObject[field.name] = field.value;
-    });
+  });
 
-    post.forEach(function (blog) {
-      blogObject[blog.name] = blog.value;
-    });
-
-    $.ajax({
+  $.ajax({
       method: 'POST',
       url: apiUrl,
       data: blogObject
-    }).done(function (data) {$('input.field').val('');
-    });
+  }).done(function (data) {
+    $('input.field, textarea.field').val(' ');
   });
 
-  var previousCount = 0;
+});
 
-  setInterval(function () {
-    $.ajax( {url: apiUrl} ).done(function (blogPosts) {
-      if(blogPosts.length > prevCount) {
+var prevCount = 0;
+setInterval(function () {
+  $.ajax( {url: apiUrl} ).done(function (blogPosts) {
+    if(blogPosts.length > prevCount) {
         prevCount = blogPosts.length;
 
-    var finishedTemplates = _.map(blogPosts, function (post) {
-      return blogTemplate(post);
-    });
+        var finishedTemplates = _.map(blogPosts, function (post) {
+          return template(new BlogPost(post));
+        });
 
-    $('.post-output').html(finishedTemplates);
+        $('.blog-output').html(finishedTemplates);
+    }
   });
 }, 1000);
-});
